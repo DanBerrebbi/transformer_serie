d42d3f0b12a4082436bba11bdd521326bc46b8b9
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-20 13:58:29 +0200

    debug sare

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -12,3 +12,4 @@ fref=$data/clean.Europarl.en-fr.fr.tst
 sort -g Europarl_josep | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
 
 sort -g Europarl_josep > test.la
+sort -g model_serie/Europarl.out_k5_alpha0.7 > test.dan
