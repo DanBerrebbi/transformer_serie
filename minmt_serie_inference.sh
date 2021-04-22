33e3d0b0a6d15fea3ea3305a4e49f1975ac0d6c7
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-22 10:49:55 +0200

    changed ckp

--- minmt_serie_inference.sh
+++ minmt_serie_inference.sh
@@ -17,7 +17,7 @@ dir=$josep/minmt_base
 dnet=$PWD/model_serie
 
 
-fmod=$dnet/network.checkpoint_00010000.pt ####### A MODIF
+fmod=$dnet/network.checkpoint_00130000.pt ####### A MODIF
 
 	for corpus in Europarl; do
 	    echo $corpus
